25401da71429fc1c7850bfff7eea78ad89ea0344
Nexus <nexusisbadateverything@gmail.com>
2024-01-02 20:49:50 +0000

    Add project files.

---
+++ OpenlabsLauncher.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenlabsLauncher.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
